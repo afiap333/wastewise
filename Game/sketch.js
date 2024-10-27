c07bd138769f1e3369a6d43bded8524e99727b37
afiap333 <115936869+afiap333@users.noreply.github.com>
2024-10-27 16:47:39 -0400

    fixed buttons

--- Game/sketch.js
+++ Game/sketch.js
@@ -1,89 +1,105 @@
-var screen=0;
-function preload(){
-  mainFont=loadFont("libraries/Poppins-Bold.ttf");
-  secondaryFont=loadFont("libraries/Poppins-Regular.ttf")
+let screen = 0;
+let mainFont, secondaryFont, startButton;
+
+function preload() {
+  mainFont = loadFont("libraries/Poppins-Bold.ttf");
+  secondaryFont = loadFont("libraries/Poppins-Regular.ttf");
 }
 
 function setup() {
-  let width=500;
-  let height=400;
+  let width = 500;
+  let height = 400;
   createCanvas(width, height);
-  startButton=new Sprite();
-  startButton.x=-1000;
-  startButton.y=-1000;
-  startButton.width=200;
-  startButton.height=50;
-  startButton.text="Click here to start";
-  startButton.color="white";
-}
-
-function draw() {
-  if(screen==0){
-    showScreen0();
-  }
-  switch(screen){
-    case 0:
-      showScreen0();
-      print(screen);
-      break;
-    case 1:
-      //showScreen1();
-      break;
-  }
-  screen=0;
-}
-
-function drawBackground(){
-  fillGradient('linear',{'steps':[[color("#AA88A2")],[color("#401F3E")]]});
-  ('linear', {
-    'from' : [20, 20], // starting point [x, y] coords
-    'to' : [580, 580], // ending point [x, y] coords
-    'steps' : [
-        [ color(255), 0 ], // p5Color Object, stopAt (0-1)
-        [ color(0, 96, 164), .25 ],
-        [ color(0, 128, 196), .75 ],
-        [ color(0), 1 ]
-    ]
-})
-  rect(0, 0, width, height);  
-}
-
-function showScreen0(){
-  drawBackground();
-  mainFontSetup();
-  text("WASTE-WISE",width/2,height/2);
-  secondaryFontSetup();
-  text("An educational game experience",width/2,height/2+50);
-  startButton.x=200;
-  startButton.y=300;
+  
+  // Create the start button
+  startButton = createButton("Click here to start");
+  startButton.position(width / 2 - 100, height / 2 + 100);
+  startButton.style("font-size", "18px");
+  startButton.style("padding", "10px 20px");
+  startButton.style("background-color", "#ffffff");
+  startButton.style("color", "#000000");
+  
+  // Set the button to change the screen when clicked
   startButton.mousePressed(() => {
     screen = 1;
   });
-  }
 
-function showScreen1(){
-  startButton.x=-1000;
-  drawBackground();
-  mainFontSetup();
-  text("Instructions",width/2,height/2-100);
-  secondaryFontSetup();
-  text("Answer which choice you think\n is the most sustainable\n and avoids overconsumption",width/2,height/2);
+  nextButton = createButton("Click here to go to question 1");
+  nextButton.position(width / 2 - 100, height / 2 + 100);
+  nextButton.style("font-size", "18px");
+  nextButton.style("padding", "10px 20px");
+  nextButton.style("background-color", "#ffffff");
+  nextButton.style("color", "#000000");
+  nextButton.hide();
 }
 
-function mainFontSetup(){
+function draw() {
+  drawBackground();
+  if (screen == 0) {
+    showScreen0();
+  } else {
+    showScreen1();
+  }
+}
+
+function drawBackground() {
+  fillGradient('linear', {
+    'from': [20, 20], 
+    'to': [580, 580],
+    'steps': [
+      [color(255), 0],
+      [color(0, 96, 164), .25],
+      [color(0, 128, 196), .75],
+      [color(0), 1]
+    ]
+  });
+  rect(0, 0, width, height);  
+}
+
+function showScreen0() {
+  drawBackground();
+  mainFontSetup();
+  text("WASTE-WISE", width / 2, height / 2);
+  secondaryFontSetup();
+  text("An educational game experience", width / 2, height / 2 + 50);
+
+  // Show the start button on screen 0
+  startButton.show();
+}
+
+function showScreen1() {
+  drawBackground();
+  mainFontSetup();
+  text("Instructions", width / 2, height / 2 - 100);
+  secondaryFontSetup();
+  text("Answer which choice you think\nis the most sustainable\nand avoids overconsumption", width / 2, height / 2);
+
+  // Hide the start button on screen 1 
+  startButton.hide();
+  nextButton.show();
+}
+
+function showQuestionScreen(){
+  drawBackground();
+  mainFontSetup();
+  text("Question 1", width / 2, height / 2 - 100);
+  secondaryFontSetup();
+  text("Question here", width / 2, height / 2);
+  // Hide the start button on screen 1 
+  startButton.hide();
+  nextButton.show();
+}
+
+function mainFontSetup() {
   textFont(mainFont);
   textAlign("center");
   fill("white");
   textSize(50);
 }
 
-function secondaryFontSetup(){
+function secondaryFontSetup() {
   textFont(secondaryFont);
   textAlign("center");
   fill("white");
   textSize(20);
-}
-
-function setUpStartButton(x,y){
-
 }
